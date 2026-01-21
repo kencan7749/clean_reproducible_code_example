5f263b495d2ab2ec065d203b9931f95bc1bc3dec
Ken <shirakawa.ken.38w@st.kyoto-u.ac.jp>
2026-01-21 11:48:16 +0900

    apply code formatting

--- emd_clust_cont_loss_sim.py
+++ emd_clust_cont_loss_sim.py
@@ -3,11 +3,28 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from sklearn.mixture import BayesianGaussianMixture
 from sklearn.model_selection import train_test_split
-from joblib import dump
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# make random seed
+np.random.seed(42)
+torch.manual_seed(42)
+
+
+# define the tiny autoencoder model
+class Encoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, embedding_dim):
+        super(Encoder, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, embedding_dim),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
 
 # setting the output directories
 output_path = "./outputs/"
@@ -15,7 +32,8 @@ output_path_data = "./outputs/data"
 output_path_graphics = "./outputs/graphics"
 output_path_models = "./outputs/models"
 # check if the output is already existed, if not create it
-list_paths = [output_path, output_path_data, output_path_graphics, output_path_models]
+list_paths = [output_path, output_path_data, output_path_graphics,
+              output_path_models]
 for outputs in list_paths:
     if os.path.isdir(outputs):
         print(f"{outputs} exists.")
@@ -23,9 +41,6 @@ for outputs in list_paths:
         print(f"{outputs} does not exist, creating it.")
         os.makedirs(outputs)
 
-# make random seed
-np.random.seed(42)
-torch.manual_seed(42)
 
 # generate (random) data
 data = np.random.randn(2000, 100)
@@ -52,33 +67,22 @@ data_train, data_test = train_test_split(data, test_size=0.2, random_state=42)
 data_train_torch = torch.from_numpy(data_train).float()
 data_test_torch = torch.from_numpy(data_test).float()
 
-# define the tiny autoencoder model
-class Encoder(nn.Module):
-    def __init__(self, input_dim, hidden_dim, embedding_dim):
-        super(Encoder, self).__init__()
-        self.layers = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim),
-            nn.ReLU(),
-            nn.Linear(hidden_dim, embedding_dim),
-        )
-
-    def forward(self, x):
-        return self.layers(x)
-
 # initialize the encoder
 encoder = Encoder(input_dim=100, hidden_dim=100, embedding_dim=50)
 print(encoder)
 # Set up the loss function and optimizer
 loss_function = nn.MSELoss()
 optimization = optim.SGD(encoder.parameters(), lr=0.001, momentum=0.9)
+
 # Initialize the lists for the losses and representations
 losses = []
 representations_during_training = []
+
 # training of the encoder
 for epoch in range(4000):
     optimization.zero_grad()
     outputs = encoder(data_train_torch)
-    loss = loss_function(outputs, data_train_torch[:, :50]) # Why :50?
+    loss = loss_function(outputs, data_train_torch[:, :50])  # Why :50?
     loss.backward()
     optimization.step()
     # save the loss
@@ -88,7 +92,9 @@ for epoch in range(4000):
     if epoch % 1000 == 0:
         with torch.no_grad():
             representations = encoder(data_train_torch)
-            representations_during_training.append(representations.cpu().numpy())
+            rep_numpy = representations.cpu().numpy()
+            representations_during_training.append(rep_numpy)
+# save the model
 torch.save(encoder, "./outputs/models/encoder.pth")
 
 # show the loss of the encoder
